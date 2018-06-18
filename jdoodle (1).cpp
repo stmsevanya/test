f9134afb1f711e68b3635cc5d12aa77d46d08fcd
Satyam Sevanya <sevanyasatyam@gmail.com>
2018-06-18 11:23:46 +0530

    Add files via upload

---
+++ jdoodle (1).cpp
@@ -0,0 +1,163 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void checkNeighbor(int M,int N, int map[][15], int visit[][15], int pt[], int neighbor[])
+{
+    int maxN=-1;
+    int i=pt[0];
+    int j=pt[1];
+    int a=-1,b=-1;
+    
+    if(i-1>=0 && visit[i-1][j]==0 && map[i-1][j]>maxN)
+    {
+        maxN=map[i-1][j];
+        a=i-1;
+        b=j;
+    }
+    
+    if(j-1>=0 && visit[i][j-1]==0 && map[i][j-1]>maxN)
+    {
+        maxN=map[i][j-1];
+        a=i;
+        b=j-1;
+    }
+    
+    if(i+1<M && visit[i+1][j]==0 && map[i+1][j]>maxN)
+    {
+        maxN=map[i+1][j];
+        a=i+1;
+        b=j;
+    }
+    
+    if(j+1<N && visit[i][j+1]==0 && map[i][j+1]>maxN)
+    {
+        maxN=map[i][j+1];
+        a=i;
+        b=j+1;
+    }
+    
+    
+    //even
+    if(j%2==0)
+    {
+        if(i-1>=0 && j-1>=0 && visit[i-1][j-1]==0 && map[i-1][j-1]>maxN)
+        {
+            maxN=map[i-1][j-1];
+            a=i-1;
+            b=j-1;
+        }
+        if(i-1>=0 && j+1<N && visit[i-1][j+1]==0 && map[i-1][j+1]>maxN)
+        {
+            maxN=map[i-1][j+1];
+            a=i-1;
+            b=j+1;
+        }
+    }
+    
+    //odd i
+    if(i%2==1)
+    {
+        if(i+1<M && j-1>=0 && visit[i+1][j-1]==0 && map[i+1][j-1]>maxN)
+        {
+            maxN=map[i+1][j-1];
+            a=i+1;
+            b=j-1;
+        }
+        if(i+1<M && j+1<N && visit[i+1][j+1]==0 && map[i+1][j+1]>maxN)
+        {
+            maxN=map[i+1][j+1];
+            a=i+1;
+            b=j+1;
+        }
+    }
+    
+    neighbor[0]=a;
+    neighbor[1]=b;
+}
+
+struct node{
+    int I;
+    int J;
+};
+
+struct list{
+    struct node* head;
+};
+
+void basestations(int M, int N, int map[][15])
+{
+    int i,j,k,l,x,y,cover;
+    int pt[2];
+    int neighbor[2];
+    int cost=0;
+    int maxCost=-1;
+    
+    struct node array[4];
+    int visit[M][15];
+    
+    for(i=0;i<M;i++)
+    {
+        for(j=0;j<N;j++)
+        {
+            visit[i][j]=0;
+            cout<<map[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    
+    for(i=0;i<M;i++)
+    {
+        for(j=0;j<N;j++)
+        {
+            for(k=0;k<4;k++)array[k].I=array[k].J=-1;
+            
+            array[0].I=i;
+            array[0].J=j;
+            visit[i][j]=1;
+            for(k=1;k<4;k++)
+            {
+                cover=-1;
+                x=-1;
+                y=-1;
+                for(l=0;l<k;l++)
+                {
+                    pt[0]=array[l].I;
+                    pt[1]=array[l].J;
+                    
+                    checkNeighbor(M,N,map,visit,pt,neighbor);
+                    
+                    if(map[neighbor[0]][neighbor[1]]>cover)
+                    {
+                        x=neighbor[0];
+                        y=neighbor[1];
+                        cover=map[neighbor[0]][neighbor[1]];
+                    }
+                }
+                array[k].I=x;
+                array[k].J=y;
+                visit[x][y]=1;
+            }
+            
+            cost=map[array[0].I][array[0].J]+map[array[1].I][array[1].J]+map[array[2].I][array[2].J]+map[array[3].I][array[3].J];
+            if(cost>maxCost)maxCost=cost;
+            cout<<i<<" "<<j<<" cost : "<<cost<<endl;
+            cout<<map[array[0].I][array[0].J]<<" "<<map[array[1].I][array[1].J]<<" "<<map[array[2].I][array[2].J]<<" "<<map[array[3].I][array[3].J]<<endl;
+            visit[array[0].I][array[0].J]=visit[array[1].I][array[1].J]=visit[array[2].I][array[2].J]=visit[array[3].I][array[3].J]=0;
+        }
+    }
+    cout<<"maxCost : "<<maxCost;
+}
+
+int main() {
+	int M,N;
+	int map[15][15];
+	cin>>M>>N;
+	for(int i=0;i<M;i++)
+	{
+	    for(int j=0;j<N;j++)
+	        cin>>map[i][j];
+	}
+	basestations(M,N,map);
+	return 0;
+}
---
+++ jdoodle.cpp
@@ -0,0 +1,158 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void openGate(int array[],int spots,int f,int g, int lastPos[])
+{
+    int select,j,k;
+    j=k=g;
+    
+    while(f)
+    {
+        while(j>=0 && array[j]>-1)j--;
+        while(k<spots && array[k]>-1)k++;
+        
+        if(j<0 || (g-j)>(k-g))select=k;
+        else if(k>=spots || (g-j)<(k-g))select=j;
+        else if(f>1)select=j;
+        else
+        {                                                  
+            lastPos[0]=j;
+            lastPos[1]=k;
+            return;
+        }
+        
+        array[select]=g;
+        f--;
+        //cout<<select<<" ";
+    }
+    lastPos[0]=select;
+    lastPos[1]=-1;
+}
+
+int estimateCost(int array[], int g[],int N)
+{
+    int cost=0;
+    int c;
+    int l;
+                for(l=0;l<N;l++)
+                {
+                    cout<<array[l]<<" ";
+                    if(array[l]<0)continue;
+                    if(array[l]==g[0])
+                        c=g[0]-l;
+                    else if(array[l]==g[1])
+                        c=(g[1]-l);
+                    else if(array[l]==g[2])
+                        c=(g[2]-l);
+                    
+                    if(c<0)c*=-1;
+                    cost+=(c+1);
+                }
+    return cost;
+}
+
+int fishing(int N, int g[], int f[])
+{
+    int minCost=100000;
+    int i,j,k,l,r1,r2,r3,cost,a1,a2,a3,b1,b2,b3;
+    int array[N];
+    for(i=0;i<N;i++)array[i]=-1;
+    int lastPos[2];
+    
+    for(i=0;i<3;i++)
+    {
+        openGate(array,N,f[i],g[i],lastPos);
+        
+            a1=lastPos[0];
+            b1=lastPos[1];
+        
+        for(r1=0;r1<2;r1++)
+        {
+            if(r1==0)array[a1]=g[i];
+            else if(b1==-1)continue;
+            else
+            {
+                array[b1]=g[i];
+                array[a1]=-1;
+            }
+            cout<<i<<" -> ";
+            for(j=0;j<3;j++)
+            {
+                if(i==j)continue;
+                
+                openGate(array,N,f[j],g[j],lastPos);
+                
+                    a2=lastPos[0];
+                    b2=lastPos[1];
+                for(r2=0;r2<2;r2++)
+                {
+                    if(r2==0)array[a2]=g[j];
+                    else if(b2==-1)continue;
+                    else
+                    {
+                        array[b2]=g[j];
+                        array[a2]=-1;
+                    }
+                    cout<<j<<" -> ";
+                    for(k=0;k<3;k++)
+                    {
+                        if(k==i || k==j)continue;
+                        
+                        openGate(array,N,f[k],g[k],lastPos);
+                        
+                            a3=lastPos[0];
+                            b3=lastPos[1];
+                            
+                        for(r3=0;r3<2;r3++)
+                        {
+                            if(r3==0)array[a3]=g[k];
+                            else if(b3==-1)continue;
+                            else
+                            {
+                                array[b3]=g[j];
+                                array[a3]=-1;
+                                cout<<"yay"<<endl;
+                            }
+                            cout<<k<<endl;
+                            //calc cost
+                            cost=estimateCost(array,g,N);
+                            cout<<endl<<cost<<endl;
+                            if(cost<minCost)minCost=cost;
+                        }
+                        
+                        for(l=0;l<N;l++)
+                        {
+                            if(array[l]==g[k])array[l]=-1;
+                        }
+                        
+                    }
+                    
+                }
+                
+                for(l=0;l<N;l++)
+                {
+                    if(array[l]==g[j])array[l]=-1;
+                }
+            }
+            
+        }
+        
+        for(l=0;l<N;l++)
+        {
+            if(array[l]==g[i])array[l]=-1;
+        }
+    }
+    return minCost;
+}
+
+int main() {
+	
+	int N=10;
+	int g[3]={3,5,9};
+	int f[3]={5,2,2};
+	int c=fishing(N,g,f);
+	cout<<"\nminCost : "<<c<<endl;
+	
+	return 0;
+}
